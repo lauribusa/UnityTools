951089a294e320e8cdb6269fdeeb316a1ffff807
LB <lauri.busa@gmail.com>
2024-11-20 17:15:23 +0100

    symlink updates need to add <==> on symlinked folders

--- U.P/Assets/_/Features/Symlink/Editor/SymlinkWindow.cs
+++ U.P/Assets/_/Features/Symlink/Editor/SymlinkWindow.cs
@@ -1,24 +1,28 @@
-using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
-using System.Text.RegularExpressions;
 using UnityEditor;
+using UnityEditor.MPE;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace _.Features.Symlink.Editor
 {
-    public class SymlinkWindow: EditorWindow
+    public class SymlinkWindow : EditorWindow
     {
         #region Public
+
         #endregion
-        
+
         #region Private & Protected
-        private const string ORIGIN_PATH = "symlink";
-        private const string LINK_PATH = @"\Assets\_";
-        private ToggleFolderGroup _folderGroup;
+
+        private static string LinkPath => $@"{Directory.GetCurrentDirectory()}\Assets\_";
+        private static string OriginPath => $@"{Directory.GetParent(Directory.GetCurrentDirectory())}\symlink";
+        private ToggleFolderGroup _rootFolderGroup;
+
         #endregion
 
         #region Unity API
+
         [MenuItem("Window/Symlink")]
         public static void ShowWindow()
         {
@@ -28,8 +32,12 @@ namespace _.Features.Symlink.Editor
 
         private void CreateGUI()
         {
-            var directoryInfo = Directory.GetParent(Directory.GetCurrentDirectory());
-            var symlinkDirectory = new DirectoryInfo(Path.Combine(directoryInfo!.FullName, ORIGIN_PATH));
+            DrawWindow();
+        }
+
+        private void DrawWindow()
+        {
+            var symlinkDirectory = new DirectoryInfo(OriginPath);
             titleContent = new GUIContent("Symlink Editor");
             rootVisualElement.style.flexShrink = 1;
             var label = new Label("Directories:")
@@ -48,45 +56,173 @@ namespace _.Features.Symlink.Editor
                     maxHeight = 500,
                 }
             };
-            var root = rootVisualElement;
-            root.Add(scrollView);
+            rootVisualElement.Add(scrollView);
             scrollView.Add(label);
-            _folderGroup = new ToggleFolderGroup(symlinkDirectory);
-            scrollView.Add(_folderGroup);
+            _rootFolderGroup = new ToggleFolderGroup(symlinkDirectory);
+            scrollView.Add(_rootFolderGroup);
             scrollView.Add(new ButtonConfirmCancelGroup(ConfirmSymlinkToggles, CancelSymlinkToggles));
+            CheckForExistingSymlinks();
         }
 
         private void ConfirmSymlinkToggles()
         {
-            var link_path = $@"{Directory.GetCurrentDirectory()}\{LINK_PATH}";
-            var origin_path = $@"{Directory.GetCurrentDirectory()}\{ORIGIN_PATH}";
-            var vals = _folderGroup.GetActivatedFoldersAndSubfolders();
-            foreach (var v in vals)
+            var folders = _rootFolderGroup.GetFoldersAndSubfolders();
+            foreach (var folder in folders)
             {
-                var targetDirectory = new DirectoryInfo($@"{link_path}\{v.Item1}");
-                var originDirectory = new DirectoryInfo($@"{origin_path}\{v.Item1}");
-                Debug.Log($"{v.Item1} : {v.Item2}");
-                Debug.Log($"link: ({targetDirectory.FullName}) {targetDirectory.Exists}");
-                Debug.Log($"origin: ({originDirectory.FullName}) {originDirectory.Exists}");
+                var linkPath = $@"{LinkPath}\{folder.Name}";
+                var linkDirectory = new DirectoryInfo(linkPath);
+                if (folder.IsToggled)
+                {
+                    if (linkDirectory.Exists)
+                    {
+                        if (!DirectoryIsSymlink(linkDirectory))
+                        {
+                            Debug.LogError($"A non-symlink directory already exists at path {linkDirectory.FullName}");
+                        }
+                        continue;
+                    }
+                    CreateSymlinkDirectory(folder.Name);
+                }
+                else
+                {
+                    if (!linkDirectory.Exists)
+                    {
+                        continue;
+                    }
+                    if (!DirectoryIsSymlink(linkDirectory))
+                    {
+                        Debug.LogError($"Directory exists but is not symlink: {linkDirectory.FullName}");
+                        continue;
+                    }
+                    DeleteSymlinkDirectory(folder.Name);
+                }
+            }
+        }
+
+        private void CheckForExistingSymlinks()
+        {
+            var folders = _rootFolderGroup.GetFoldersAndSubfolders();
+            foreach (var folder in folders)
+            {
+                var targetDirectory = new DirectoryInfo($@"{LinkPath}\{folder.Name}");
+                var isValidSymlink = targetDirectory.Exists && DirectoryIsSymlink(targetDirectory);
+                if (!isValidSymlink) continue;
+                var foundFolder = _rootFolderGroup.GetFolder(folder.Name);
+                if (foundFolder == null)
+                {
+                    Debug.LogError($"Couldn't find Symlink Folder with name: {folder.Name} in FolderGroup.");
+                    continue;
+                }
+
+                foundFolder.SetToggleState(true);
             }
         }
 
         private void CancelSymlinkToggles()
         {
-            Debug.Log($"Canceled");
+            rootVisualElement.Clear();
+            DrawWindow();
         }
 
-        private void RunCommand(string linkPath, string targetPath)
+        private bool DirectoryIsSymlink(DirectoryInfo directoryInfo)
         {
-            var process = new System.Diagnostics.Process();
-            var startInfo = new System.Diagnostics.ProcessStartInfo
+            return directoryInfo.Attributes.HasFlag(FileAttributes.ReparsePoint);
+        }
+
+        private void DeleteSymlinkDirectory(string path)
+        {
+            var linkPath = $@"{LinkPath}\{path}";
+            var linkDirectory = new DirectoryInfo(linkPath);
+            Debug.Log($"to delete: {linkDirectory.FullName}");
+            Directory.Delete(linkPath);
+        }
+        private void CreateSymlinkDirectory(string path)
+        {
+            var linkPath = $@"{LinkPath}\{path}";
+            var originPath = $@"{OriginPath}\{path}";
+            var linkDirectory = new DirectoryInfo(linkPath);
+            var originDirectory = new DirectoryInfo(originPath);
+            var subFolders = path.Split('\\');
+            var nestedPath = "";
+            for (int i = 0; i < subFolders.Length - 1; i++)
             {
-                WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
-                FileName = "cmd.exe",
-                Arguments = $@"mklink /J \{linkPath} \{targetPath}"
+                var subFolder = subFolders[i];
+                nestedPath += $@"\{subFolder}";
+                var nestedDirectory = new DirectoryInfo(linkPath);
+                if (nestedDirectory.Exists) continue;
+                Directory.CreateDirectory($"{LinkPath}{nestedPath}");
+            }
+            if (linkDirectory.Exists)
+            {
+                if (DirectoryIsSymlink(linkDirectory))
+                {
+                    Debug.LogWarning($"Symlink directory already exists: {linkDirectory.FullName}");
+                    return;
+                }
+
+                Debug.LogError($"A non-symlink directory already exists at path {linkDirectory.FullName}");
+                return;
+            }
+
+            if (!originDirectory.Exists)
+            {
+                EditorUtility.DisplayDialog("Error",
+                    $"Could not find a valid directory at path {originDirectory.FullName}", "OK");
+                return;
+            }
+            var success = MakeSymlinkFromPowerShell(linkPath, originPath);
+
+            Debug.Log(success
+                ? $"Created Symlink Directory at {linkPath} from {originPath}"
+                : $"Failed to create Symlink Directory at {linkPath} from {originPath}.");
+        }
+        
+        private bool MakeSymlinkFromCommandLine(string linkPath, string originPath)
+        {
+            var args = $"/c mklink /J {linkPath} {originPath}";
+            return RunProcess(args, "cmd");
+        }
+
+        private bool MakeSymlinkFromPowerShell(string linkPath, string originPath)
+        {
+            var args = $"New-Item -Path {linkPath} -ItemType Junction -Value {originPath}";
+            return RunProcess(args, "powershell");
+        }
+
+        private static bool RunProcess(string args, string processName)
+        {
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden,
+                    FileName = $"{processName}.exe",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    Arguments = args
+                }
             };
-            process.StartInfo = startInfo;
             process.Start();
+            var stdout = process.StandardOutput.ReadToEnd();
+            var stderr = process.StandardError.ReadToEnd();
+            if(stdout.Length > 0)Debug.Log($"{stdout} || END.");
+            if(stderr.Length > 0)Debug.LogError($"{stderr} || END.");
+            process.WaitForExit();
+            return process.ExitCode == 0;
+        }
+
+        private void PrintAllSymlinkDir(DirectoryInfo directoryInfo)
+        {
+            if (DirectoryIsSymlink(directoryInfo))
+            {
+                Debug.Log($"Is Symlink: {directoryInfo.FullName}");
+            }
+
+            foreach (var dir in directoryInfo.GetDirectories())
+            {
+                PrintAllSymlinkDir(dir);
+            }
         }
         #endregion
     }
--- U.P/Assets/_/Features/Symlink/Editor/SymlinkWindowEditorComponents.cs
+++ U.P/Assets/_/Features/Symlink/Editor/SymlinkWindowEditorComponents.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,7 +11,9 @@ namespace _.Features.Symlink.Editor
     public class ToggleFolder : VisualElement
     {
         public bool IsActive { get; private set; }
-        public string FullPath => name;
+        public string Name => name;
+        private readonly Toggle _toggle;
+        public bool IsToggled => _toggle.value;
         public ToggleFolder(string directoryName, string unicodeSymbol = "", string parentPath = null)
         {
             name = parentPath != null ? $@"{parentPath}\{directoryName}" : directoryName;
@@ -18,22 +21,27 @@ namespace _.Features.Symlink.Editor
             {
                 Add(new Label($"{unicodeSymbol}"));
             }
-            var toggle = new Toggle
+            _toggle = new Toggle
             {
                 style =
                 {
                     marginRight = 20
                 }
             };
-            toggle.RegisterCallback<ChangeEvent<bool>>(OnToggle);
+            _toggle.RegisterCallback<ChangeEvent<bool>>(OnToggle);
             style.flexDirection = FlexDirection.Row;
-            Add(toggle);
+            Add(_toggle);
             Add(new Label($"{directoryName}"));
         }
 
         private void OnToggle(ChangeEvent<bool> evt)
         {
             IsActive = evt.newValue;
+        }
+
+        public void SetToggleState(bool value)
+        {
+            _toggle.value = value;
         }
     }
 
@@ -43,6 +51,7 @@ namespace _.Features.Symlink.Editor
         private readonly List<ToggleFolder> _folders = new();
         private List<ToggleFolderGroup> _subFoldersGroup = new();
         private string parentPath = string.Empty;
+        public ToggleFolderGroup Parent { get; private set; }
         public ToggleFolderGroup(DirectoryInfo parent, ToggleFolderGroup root = null, string parentPath = null)
         {
             style.marginLeft = 15;
@@ -58,10 +67,37 @@ namespace _.Features.Symlink.Editor
                 var subFolderGroup = new ToggleFolderGroup(subDirectoryInfo, this, subFolderPath);
                 Add(subFolderGroup);
                 _subFoldersGroup.Add(subFolderGroup);
+                if (root != null)
+                {
+                    Parent = root;
+                }
             }
         }
 
-        public Tuple<string, bool>[] GetActivatedFoldersAndSubfolders()
+        /// <summary>
+        /// Will return a ToggleFolder from within its elements and sub-elements with the specified name.
+        /// </summary>
+        /// <param name="folderName">The name of the ToggleFolder to find</param>
+        /// <returns>Returns null if none are found.</returns>
+        public ToggleFolder GetFolder(string folderName)
+        {
+            var allFolders = new List<ToggleFolder>();
+            allFolders.AddRange(_folders);
+            foreach (var subFolderGroup in _subFoldersGroup)
+            {
+                var subFolders = subFolderGroup._folders;
+                allFolders.AddRange(subFolders);
+            }
+
+            foreach (var subFolder in allFolders)
+            {
+                if (subFolder.name != folderName) continue;
+                return subFolder;
+            }
+            return null;
+        }
+
+        public ToggleFolder[] GetFoldersAndSubfolders()
         {
             var allFolders = new List<ToggleFolder>();
             allFolders.AddRange(_folders);
@@ -70,12 +106,7 @@ namespace _.Features.Symlink.Editor
                 var subFolders = subFolderGroup._folders.ToArray();
                 allFolders.AddRange(subFolders);
             }
-            var activatedFoldersAndSubfolders = new Tuple<string, bool>[allFolders.Count];
-            for (int i = 0; i < activatedFoldersAndSubfolders.Length; i++)
-            {
-                activatedFoldersAndSubfolders[i] = new Tuple<string, bool>(allFolders[i].name, allFolders[i].IsActive);
-            }
-            return activatedFoldersAndSubfolders;
+            return allFolders.ToArray();
         }
     }
 
